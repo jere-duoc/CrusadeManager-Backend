170b8f69afdb985fa015e4cdf05f13b6eb155f2f
jere <jer.silvaa@duocuc.cl>
2025-11-10 11:11:52 -0300

    Add files via upload

---
+++ Crusade-Manager/src/main/java/Crusade/Manager/Crusade/Manager/CrusadeManagerApplication.java
@@ -0,0 +1,13 @@
+package Crusade.Manager.Crusade.Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrusadeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrusadeManagerApplication.class, args);
+	}
+
+}
---
+++ Crusade-Manager/src/test/java/Crusade/Manager/Crusade/Manager/CrusadeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package Crusade.Manager.Crusade.Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrusadeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
